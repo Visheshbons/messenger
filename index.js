a24d012ecf398a01747b20bb556d8fcef3ff04b3
Vishesh Kudva <vishesh.kudva@outlook.com>
2025-04-18 10:22:53 +1200

    Added more code

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('This site does not exist yet');
+    res.status(200).render('index.ejs', {
+        condition: '1 === 1',
+    });
 }).use((req, res, next) => {
-    res.status(404).send('<pre>ERR_404_NOT_FOUND: The requested URL was not found on this server.</pre>');
+    res.status(404).send('<pre>ERR_404_NOT_FOUND</pre>');
 }).use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send('<pre>ERR_500_INTERNAL_SERVER_ERROR: Something went wrong on the server.</pre>');
+    res.status(500).send('<pre>ERR_500_INTERNAL_SERVER_ERROR</pre>');
 });
 
 app.listen(port, () => {
